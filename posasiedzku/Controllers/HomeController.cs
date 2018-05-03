8f0ec4817f0e14b11b64ee48734c5baa09ec18e0
Michał Mamełka <michalmamelka@outlook.com>
2018-05-03 17:56:17 +0200

    brak

--- posasiedzku/Controllers/HomeController.cs
+++ posasiedzku/Controllers/HomeController.cs
@@ -12,7 +12,7 @@ namespace posasiedzku.Controllers
         {
             return View();
         }
-
+      
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
